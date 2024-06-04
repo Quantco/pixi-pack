f87e442834446648ead53bf04ed9f0fb4b351f0a
Daniel Elsner <daniel.elsner@quantco.com>
2024-06-04 10:07:25 +0200

    Add serialization tests

--- src/lib.rs
+++ src/lib.rs
@@ -11,7 +11,7 @@ pub const PIXI_PACK_METADATA_PATH: &str = "pixi-pack.json";
 pub const DEFAULT_PIXI_PACK_VERSION: &str = "1";
 
 /// The metadata for a "pixi-pack".
-#[derive(Serialize, Deserialize, Debug, Clone)]
+#[derive(Serialize, Deserialize, Debug, Clone, PartialEq, Eq)]
 pub struct PixiPackMetadata {
     /// The pack format version.
     pub version: String,
@@ -25,5 +25,38 @@ impl Default for PixiPackMetadata {
             version: DEFAULT_PIXI_PACK_VERSION.to_string(),
             platform: Platform::current(),
         }
+    }
+}
+
+/* --------------------------------------------------------------------------------------------- */
+/*                                             TESTS                                             */
+/* --------------------------------------------------------------------------------------------- */
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use rstest::*;
+    use serde_json::{json, Value};
+
+    #[rstest]
+    fn test_metadata_serialization() {
+        let metadata = PixiPackMetadata {
+            version: DEFAULT_PIXI_PACK_VERSION.to_string(),
+            platform: Platform::Linux64,
+        };
+        let result = json!(metadata).to_string();
+        assert_eq!(result, "{\"version\":\"1\",\"platform\":\"linux-64\"}");
+        assert_eq!(
+            serde_json::from_str::<PixiPackMetadata>(&result).unwrap(),
+            metadata
+        );
+    }
+
+    #[rstest]
+    #[case(json!({"version": "1", "platform": "linux64"}))]
+    #[case(json!({"version": 1.0, "platform": "linux-64"}))]
+    #[case(json!({"version": 1, "platform": "linux-64"}))]
+    fn test_metadata_serialization_failure(#[case] invalid: Value) {
+        assert!(serde_json::from_str::<PixiPackMetadata>(&invalid.to_string()).is_err());
     }
 }
